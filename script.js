a5e6f4554e12bf0fbc579507ddc1f2c0e0666dd3
ajim9075shaikh <142928027+ajim9075shaikh@users.noreply.github.com>
2024-06-03 16:14:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 //complete this code
+// Class definition for Person
 class Person {
-	constructor(name, age){
-		this.name=name;
-		this.age=age;
-	}
-	get name(){
-		return this.name
-	}
-	set age(new age) {
-		return this.age = new age;
-	}
-	 get age() {
-        return this.age;
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+
+    // Getter for name
+    get name() {
+        return this._name;
+    }
+
+    // Setter for age
+    set age(newAge) {
+        this._age = newAge;
+    }
+
+    // Getter for age to verify age change (optional, for testing purposes)
+    get age() {
+        return this._age;
     }
 }
 
+// Subclass definition for Student
 class Student extends Person {
-	
-	study(){
-	 console.log(`${this.name} is studying`);
+    study() {
+        console.log(`${this.name} is studying`);
+    }
 }
 
+// Subclass definition for Teacher
 class Teacher extends Person {
-	teach(){
-		 console.log(`${this.name} is teaching`);
-	}
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
 }
 
-// Do not change the code below this line
+
+
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
